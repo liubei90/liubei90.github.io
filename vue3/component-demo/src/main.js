8ad35b19eb23ec3dca4ead5d8ee24c9784c2d8ff
liubei90 <1021086660@qq.com>
2022-03-16 09:26:48 +0800

    3-16

--- vue3/component-demo/src/main.js
+++ vue3/component-demo/src/main.js
@@ -1,16 +1,33 @@
 /*
  * @Author: liubei
  * @Date: 2021-11-14 18:48:30
- * @LastEditTime: 2021-11-14 18:48:54
+ * @LastEditTime: 2022-03-11 11:44:29
  * @Description: 
  */
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
 const app = new Vue({
     el: '#app',
-    render: (h) => {
-        return h(App)
+    data() {
+        return { count: 1 }
+    },
+    beforeCreate() {
+        console.log('setTimeout1', this.count) // undefined
+
+        setTimeout(() => {
+            console.log('setTimeout2', this.count = this.count + 1) // 2
+        }, 0)
+
+        let n= 0
+        for (let i = 0; i < 10000000000000; i++) {
+            n++
+        }
+        console.log(n)
+    },
+    render(h) {
+        return h('div', this.count) // 2
+        // return h(App)
     }
 
 })
